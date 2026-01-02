402e5d7d24c886bc6fcb6fa9e417b7f395e8b4fc
Adrien Heisch <adrienheisch@orange.fr>
2026-01-02 15:22:18 +0100

    add sveltekit project

---
+++ www/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+  import "../app.css";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link class="bg-amber-50" rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
