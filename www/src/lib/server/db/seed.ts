4588dc372dfa6549e990aef859ce0a774d168eb9
Adrien Heisch <adrienheisch@orange.fr>
2026-01-13 01:08:07 +0100

    more entries in seeded database, always reset

--- www/src/lib/server/db/seed.ts
+++ www/src/lib/server/db/seed.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { seed } from "drizzle-seed";
+import { reset, seed } from "drizzle-seed";
 import postgres from "postgres";
 import * as schema from "./schema";
 
@@ -7,13 +7,19 @@ export default async function seedDb() {
   if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
   const client = postgres(process.env.DATABASE_URL);
   const db = drizzle(client, { schema });
+  await reset(db, schema);
   await seed(db, schema).refine((gen) => ({
+    session: {
+      count: 30,
+    },
     user: {
+      count: 20,
       columns: {
         bio: gen.loremIpsum(),
       },
     },
     pet: {
+      count: 40,
       columns: {
         species: gen.valuesFromArray({
           values: ["Cat", "Dog", "Cow", "Fish", "Horse"],
@@ -23,12 +29,17 @@ export default async function seedDb() {
         }),
       },
     },
+    friendsPair: {
+      count: 100,
+    },
     chatMessage: {
+      count: 2000,
       columns: {
         content: gen.loremIpsum(),
       },
     },
     post: {
+      count: 400,
       columns: {
         content: gen.loremIpsum(),
       },
