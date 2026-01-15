4c0daf94fee001b8127fc1770512b06b8dceecd0
Adrien Heisch <adrienheisch@orange.fr>
2026-01-15 02:12:25 +0100

    fix production build

--- www/src/lib/server/db/index.ts
+++ www/src/lib/server/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "$env/dynamic/private";
 import * as schema from "./schema";
+import { building } from "$app/environment";
 
 if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
@@ -9,8 +10,8 @@ const client = postgres(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
 
-console.log("Adding trigger");
-await db.execute(`
+if (!building) {
+  await db.execute(`
 CREATE OR REPLACE FUNCTION canonicalize_friends_pair() RETURNS trigger AS $$
 DECLARE
   temp_left UUID;
@@ -27,5 +28,5 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE TRIGGER trg_canonicalize_friends_pair
 BEFORE INSERT OR UPDATE ON friends_pair
 FOR EACH ROW EXECUTE FUNCTION canonicalize_friends_pair();
-`);
-console.log("Trigger added");
+  `);
+}
