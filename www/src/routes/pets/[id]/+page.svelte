3efb0f6f1d7c7d934da675a2d95b61f7b5f3116f
Adrien Heisch <adrienheisch@orange.fr>
2026-01-21 00:10:12 +0100

    use server load function

--- www/src/routes/pets/[id]/+page.svelte
+++ www/src/routes/pets/[id]/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
-
-  let { data }: { data: PageData } = $props();
+  const { data } = $props();
 
   // Dérivation automatique de l'animal depuis data
   let animal = $derived(data.animal);
