5b54213efbd4e595e5baa4a196748b54138775a3
Maia <mszymcza@student.42.fr>
2026-01-20 13:01:14 +0100

    english trad

--- www/src/routes/list_pets/+page.server.ts
+++ www/src/routes/list_pets/+page.server.ts
@@ -3,7 +3,7 @@ import { db } from "$lib/server/db";
 import * as table from "$lib/server/db/schema";
 
 export const load: PageServerLoad = async () => {
-  // Récupérer tous les animaux depuis la base de données
+  // Fetch all animals from the database
   const pets = await db.select().from(table.pet);
   
   return {
