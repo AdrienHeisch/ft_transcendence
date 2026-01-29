c45947a55ecf51ad8831d662fbd12cf9ae30ee23
Adrien Heisch <adrienheisch@orange.fr>
2026-01-29 20:37:29 +0100

    better validation

--- www/src/lib/associations.remote.ts
+++ www/src/lib/associations.remote.ts
@@ -7,21 +7,25 @@ import { db } from "./server/db";
 export const getAssociations = query(
   z.object({
     search: z.string(),
-    type: z.string().nullable(),
+    type: schema.associationTypeSchema.nullable(),
     city: z.string().nullable(),
-    sortBy: z.custom<"name" | "type" | "city" | "animalsCount">(),
+    sortBy: z.custom<"name" | "type">(),
   }),
-  ({ search, type, city, sortBy }) => {
+  ({ search, type, sortBy }) => {
     return db
       .select()
       .from(schema.association)
       .where(
         and(
           search ? ilike(schema.association.name, `%${search}%`) : undefined,
-          city ? eq(schema.association.city, city) : undefined,
+          // city ? eq(schema.association.city, city) : undefined,
           type ? eq(schema.association.type, type) : undefined,
         ),
       )
       .orderBy(getTableColumns(schema.association)[sortBy]);
   },
 );
+
+export const getPetsCount = query(z.string(), (id) => {
+  return db.$count(schema.pet, eq(schema.pet.ownerId, id));
+});
