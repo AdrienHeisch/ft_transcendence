416e8362675f9710186674ba9957bbd56768a726
Adrien Heisch <adrienheisch@orange.fr>
2026-02-15 19:10:26 +0100

    add background color to /feed

--- www/src/routes/feed/+page.svelte
+++ www/src/routes/feed/+page.svelte
@@ -9,10 +9,10 @@ const posts = $derived(await getPosts({}));
 </script>
 
 <!-- Zone de contenu principal -->
-<main class="flex-1 p-6">
+<main class="flex-1 p-6 bg-linear-to-br from-orange-50 via-yellow-50 to-orange-100">
 	<div class="max-w-2xl mx-auto">
     <!-- Fil d'actualité -->
-		<section class="bg-white rounded-xl shadow-lg border-3 border-orange-400 p-6 mb-6">
+		<section class="bg-orange-50 rounded-xl shadow-lg border-3 border-orange-400 p-6 mb-6">
 			<h2 class="text-2xl font-bold text-orange-900 mb-4 flex items-center gap-2">
 				<span>📰</span>
 				News Feed
