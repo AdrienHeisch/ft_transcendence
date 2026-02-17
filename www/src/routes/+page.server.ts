707095b044145d80e78e68adbb08dcb2dc014a43
Maia <mszymcza@student.42.fr>
2026-02-17 10:17:24 +0100

    home page new style

--- www/src/routes/+page.server.ts
+++ www/src/routes/+page.server.ts
@@ -1,3 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 
-export const load = () => redirect(302, "/feed");
+export const load = ({ locals }) => {
+  // Redirect authenticated users to feed
+  if (locals.currentUser) {
+    redirect(302, "/feed");
+  }
+  // Show homepage for guests
+};
