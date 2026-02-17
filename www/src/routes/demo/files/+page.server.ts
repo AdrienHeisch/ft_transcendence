4d1bff8d3b24a75824779eaca358bc12244113ec
Adrien Heisch <adrienheisch@orange.fr>
2026-02-17 02:21:30 +0100

    remove all /demo routes

--- www/src/routes/demo/files/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { PublicStorage } from "$lib/server/storage";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = () => {
-  return {
-    files: PublicStorage.list({ prefix: "demo/", delimiter: "/" }),
-  };
-};
--- www/src/routes/demo/private-files/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { PrivateStorage } from "$lib/server/storage";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = () => {
-  return {
-    files: PrivateStorage.list({ prefix: "demo/", delimiter: "/" }),
-  };
-};
