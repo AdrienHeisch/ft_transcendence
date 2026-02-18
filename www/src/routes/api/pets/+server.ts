b9a7d0421f9e7eb559da0fea77728ef66813586f
Adrien Heisch <adrienheisch@orange.fr>
2026-02-18 15:01:11 +0100

    add filtering and sorting to GET api routes

--- www/src/routes/api/pets/+server.ts
+++ www/src/routes/api/pets/+server.ts
@@ -1,7 +1,17 @@
+import { error } from "@sveltejs/kit";
+import z from "zod";
 import { getPets } from "$lib/pets.remote";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = async () => {
-  const pets = await getPets({ search: "", sortBy: "name" });
+export const GET: RequestHandler = async ({ url }) => {
+  const search = url.searchParams.get("q") ?? "";
+  const owner = url.searchParams.get("owner") ?? undefined;
+  const species = url.searchParams.get("species") ?? undefined;
+  const sortByParse = z.safeParse(
+    z.enum(["name", "species"]),
+    url.searchParams.get("sortBy") ?? "name",
+  );
+  const sortBy = sortByParse.success ? sortByParse.data : error(400);
+  const pets = await getPets({ search, owner, species, sortBy });
   return new Response(JSON.stringify(pets));
 };
--- www/src/routes/api/posts/+server.ts
+++ www/src/routes/api/posts/+server.ts
@@ -1,7 +1,13 @@
 import { getPosts } from "$lib/posts.remote";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = async () => {
-  const posts = await getPosts({});
+export const GET: RequestHandler = async ({ url }) => {
+  const author = url.searchParams.get("author") ?? undefined;
+  const pet = url.searchParams.get("pet") ?? undefined;
+  // TODO sortBy
+  const posts = (await getPosts({ author, pet })).map((post) => ({
+    ...post,
+    author: post.author.id,
+  }));
   return new Response(JSON.stringify(posts));
 };
