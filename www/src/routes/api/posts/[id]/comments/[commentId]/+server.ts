27d07e9177a74bd76dd37ab5cfb3d1678dfee2ad
Adrien Heisch <adrienheisch@orange.fr>
2026-02-19 00:46:13 +0100

    add post comments api

--- www/src/routes/api/posts/[id]/comments/[commentId]/+server.ts
+++ www/src/routes/api/posts/[id]/comments/[commentId]/+server.ts
@@ -1,9 +1,71 @@
-import { getPostComments } from "$lib/posts.remote";
+import { error } from "@sveltejs/kit";
+import { and, eq } from "drizzle-orm";
+import z from "zod";
+import { getApiUser } from "$lib/server/auth";
+import { db } from "$lib/server/db";
+import * as schema from "$lib/server/db/schema";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = async ({ params: { id, commentId } }) => {
-  const [comment] = (await getPostComments(id)).filter(
-    (comment) => comment.id === commentId,
-  );
+export const GET: RequestHandler = async ({ params: { commentId: id } }) => {
+  const comment = await db
+    .select()
+    .from(schema.postComment)
+    .where(eq(schema.postComment.id, id));
   return new Response(JSON.stringify(comment));
+};
+
+export const PUT: RequestHandler = async ({
+  params: { commentId: id },
+  request,
+}) => {
+  const user = getApiUser();
+  // if (!z.uuid().safeParse(id).success) {
+  //   error(404);
+  // }
+  const formData = Object.fromEntries((await request.formData()).entries());
+  const parsed = z
+    .object({
+      content: z.string(),
+    })
+    .strict()
+    .safeParse(formData);
+  if (parsed.success) {
+    const updated = (
+      await db
+        .update(schema.postComment)
+        .set(parsed.data)
+        .where(
+          and(
+            eq(schema.postComment.id, id),
+            eq(schema.postComment.author, user.id),
+          ),
+        )
+        .returning()
+    ).at(0);
+    return new Response(JSON.stringify(updated));
+  } else {
+    error(400, parsed.error);
+  }
+};
+
+export const DELETE: RequestHandler = async ({ params: { commentId: id } }) => {
+  const user = getApiUser();
+  // if (!z.uuid().safeParse(id).success) {
+  //   error(404);
+  // }
+  const deleted = (
+    await db
+      .delete(schema.postComment)
+      .where(
+        and(
+          eq(schema.postComment.id, id),
+          eq(schema.postComment.author, user.id),
+        ),
+      )
+      .returning()
+  ).at(0);
+  if (!deleted) {
+    error(404);
+  }
+  return new Response();
 };
