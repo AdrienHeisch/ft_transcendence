dc4bbc180694336058e432c799ce1bfa51d2f8a8
Adrien Heisch <aheisch@student.42.fr>
2026-02-20 16:42:09 +0100

    seed pet avatars

--- www/src/lib/server/storage/seed.ts
+++ www/src/lib/server/storage/seed.ts
@@ -1,16 +1,33 @@
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { POST_IMAGE_PREFIX, USER_AVATAR_PREFIX } from "../../storage";
+import {
+  PET_AVATAR_PREFIX,
+  POST_IMAGE_PREFIX,
+  USER_AVATAR_PREFIX,
+} from "../../storage";
 import * as schema from "../db/schema";
 import { PublicStorage } from "../storage";
 import petImages from "./pet-images.json";
 
 const allSpecies = Object.keys(petImages) as (keyof typeof petImages)[];
 
-function randomImage(species: keyof typeof petImages): string {
+function randomImage(pet?: schema.Pet): string {
+  const species =
+    pet && pet.species in petImages
+      ? (pet.species as keyof typeof petImages)
+      : allSpecies[Math.floor(Math.random() * allSpecies.length)];
   const images = petImages[species];
   return images[Math.floor(Math.random() * images.length)];
+}
+
+async function uploadPetImage(key: string, pet?: schema.Pet) {
+  const fileExists = await PublicStorage.exists(key);
+  if (!fileExists) {
+    const imageUrl = randomImage(pet);
+    const file = await fetch(imageUrl);
+    await PublicStorage.upload(key, await file.blob(), "image/jpeg");
+  }
 }
 
 export default async function seedStorage() {
@@ -30,20 +47,16 @@ export default async function seedStorage() {
     }
   }
   const pets = await db.select().from(schema.pet);
-  const petById = new Map(pets.map((p) => [p.id, p]));
-  for (const post of await db.select().from(schema.post)) {
-    const key = POST_IMAGE_PREFIX + post.id;
-    const fileExists = await PublicStorage.exists(key);
-    if (!fileExists) {
-      const pet = post.pet ? petById.get(post.pet) : null;
-      const species =
-        pet && pet.species in petImages
-          ? (pet.species as keyof typeof petImages)
-          : allSpecies[Math.floor(Math.random() * allSpecies.length)];
-      const imageUrl = randomImage(species);
-      const file = await fetch(imageUrl);
-      await PublicStorage.upload(key, await file.blob(), "image/jpeg");
-    }
+  for (const pet of pets) {
+    await uploadPetImage(PET_AVATAR_PREFIX + pet.id, pet);
+  }
+  await db.update(schema.pet).set({ hasAvatar: true });
+  const posts = await db
+    .select({ ...getTableColumns(schema.post), pet: schema.pet })
+    .from(schema.post)
+    .leftJoin(schema.pet, eq(schema.post.pet, schema.pet.id));
+  for (const post of posts) {
+    await uploadPetImage(POST_IMAGE_PREFIX + post.id, post.pet ?? undefined);
   }
   console.log("Done");
   client.end();
