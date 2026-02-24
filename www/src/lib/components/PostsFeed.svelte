87a0edb9099df506f8a4a73727a82db776c63f0e
Maia <mszymcza@student.42.fr>
2026-02-24 18:24:48 +0100

    indicateur de chargement (feed)

--- www/src/lib/components/PostsFeed.svelte
+++ www/src/lib/components/PostsFeed.svelte
@@ -20,6 +20,7 @@ const {
 let offset = $state(0);
 let loader = $state<HTMLDivElement>();
 let isLoaderVisible = $state(false);
+let isLoadingMore = $state(false);
 
 let posts = $derived(
   await getPosts({ offset: 0, limit: PAGE_SIZE, ...queryArgs }),
@@ -27,12 +28,13 @@ let posts = $derived(
 
 async function onLoaderVisibilityChange(isVisible: boolean) {
   if (isVisible) {
-    if (!isLoaderVisible) {
+    if (!isLoaderVisible && !isLoadingMore) {
       isLoaderVisible = true;
+      isLoadingMore = true;
       offset += PAGE_SIZE;
-      posts = posts.concat(
-        await getPosts({ offset, limit: PAGE_SIZE, ...queryArgs }),
-      );
+      const newPosts = await getPosts({ offset, limit: PAGE_SIZE, ...queryArgs });
+      posts = posts.concat(newPosts);
+      isLoadingMore = false;
     }
   } else {
     isLoaderVisible = false;
@@ -60,4 +62,14 @@ $effect(() => {
       <Post post={post} author={author} currentUser={currentUser} />
     {/if}
   {/each}
+  
+  {#if isLoadingMore}
+    <div class="flex justify-center items-center py-8">
+      <div class="flex flex-col items-center gap-3">
+        <div class="w-12 h-12 border-4 border-orange-400 border-t-orange-600 rounded-full animate-spin"></div>
+        <p class="text-orange-700 font-medium">Loading more posts...</p>
+      </div>
+    </div>
+  {/if}
+  
   <div bind:this={loader}></div>
