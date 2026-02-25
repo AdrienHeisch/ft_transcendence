d67a25422d9ee345668610eaf58cb9b0d0723df4
Adrien Heisch <adrienheisch@orange.fr>
2026-02-25 01:05:37 +0100

    add rate limiting to /feed
    fixes a weird warning, and good practice in general I guess

--- www/src/lib/components/PostsFeed.svelte
+++ www/src/lib/components/PostsFeed.svelte
@@ -4,7 +4,7 @@ import { getPosts } from "$lib/posts.remote";
 import { type User } from "$lib/server/db/schema";
 import { getUser } from "$lib/user.remote";
 
-const PAGE_SIZE = 4;
+const PAGE_SIZE = 10;
 
 const {
   queryArgs = {},
@@ -21,6 +21,7 @@ let offset = $state(0);
 let loader = $state<HTMLDivElement>();
 let isLoaderVisible = $state(false);
 let isLoadingMore = $state(false);
+let nothingToLoad = $state(false);
 
 let posts = $derived(
   await getPosts({ offset: 0, limit: PAGE_SIZE, ...queryArgs }),
@@ -28,7 +29,8 @@ let posts = $derived(
 
 async function onLoaderVisibilityChange(isVisible: boolean) {
   if (isVisible) {
-    if (!isLoaderVisible && !isLoadingMore) {
+    if (!isLoaderVisible) {
+      await wait();
       isLoaderVisible = true;
       isLoadingMore = true;
       offset += PAGE_SIZE;
@@ -37,12 +39,21 @@ async function onLoaderVisibilityChange(isVisible: boolean) {
         limit: PAGE_SIZE,
         ...queryArgs,
       });
+      nothingToLoad = newPosts.length === 0;
       posts = posts.concat(newPosts);
-      isLoadingMore = false;
+      setTimeout(() => (isLoadingMore = false), 1000);
     }
   } else {
     isLoaderVisible = false;
   }
+}
+
+async function wait() {
+  if (!isLoadingMore) {
+    return;
+  }
+  await new Promise((r) => setTimeout(r, 100));
+  return await wait();
 }
 
 $effect(() => {
@@ -60,20 +71,20 @@ $effect(() => {
 });
 </script>
 
-  {#each posts as post}
-    {@const author = await getUser(post.author)}
-    {#if author}
-      <Post post={post} author={author} currentUser={currentUser} />
-    {/if}
-  {/each}
-  
-  {#if isLoadingMore}
-    <div class="flex justify-center items-center py-8">
-      <div class="flex flex-col items-center gap-3">
-        <div class="w-12 h-12 border-4 border-orange-400 border-t-orange-600 rounded-full animate-spin"></div>
-        <p class="text-orange-700 font-medium">Loading more posts...</p>
-      </div>
-    </div>
+{#each posts as post}
+  {@const author = await getUser(post.author)}
+  {#if author}
+    <Post post={post} author={author} currentUser={currentUser} />
   {/if}
-  
-  <div bind:this={loader}></div>
+{/each}
+
+{#if !nothingToLoad}
+  <div class="flex justify-center items-center py-8">
+    <div class="flex flex-col items-center gap-3">
+      <div class="w-12 h-12 border-4 border-orange-400 border-t-orange-600 rounded-full animate-spin"></div>
+      <p class="text-orange-700 font-medium">Loading more posts...</p>
+    </div>
+  </div>
+{/if}
+
+<div bind:this={loader}></div>
